3cac0b1b378ba1095c51219eace3f62a64a1a668
Maryam-n98 <mariam.mohannad98@gmail.com>
2021-05-18 23:52:47 +0300

    finish 30% of lab

---
+++ src/copmonents/SelectedBeast.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class SelectedBeast extends React.Component{
+
+    
+    render(){
+        return(
+            <>
+            
+           <p>{this.props.SelectedBeastContent}</p>
+            
+            </>
+        )
+
+        
+    }
+}
+
+export default SelectedBeast ;
