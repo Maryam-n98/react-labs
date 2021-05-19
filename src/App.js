4c51975112a733804daecd97df4a1f1ee83fcab7
Maryam-n98 <mariam.mohannad98@gmail.com>
2021-05-19 20:35:10 +0300

    lab03

--- src/App.js
+++ src/App.js
@@ -1,41 +1,65 @@
-import React from 'react';
 import Header from './copmonents/Header';
 import Main from './copmonents/Main';
 import Footer from './copmonents/Footer';
+import React from 'react';
+import hornedDataApp from './copmonents/HornedData.json';
 import SelectedBeast from './copmonents/SelectedBeast';
-constructor(props){
-  super(props)
-  this.state={
-    
-      hornedData:hornedDataApp,
-      SelectedBeastContentFromState:'hello from states and props'
+// import HornedBeast from './copmonents/HornedBeast';
 
-  }
+
+class App extends React.Component{
+
+  constructor(props){
+    super(props)
+    this.state={
+      
+        hornedData:hornedDataApp,
+        SelectedBeastContentFromState: '',
+        SelectedBeastContentFromState2: '',
+        SelectedBeastContentFromState3: '',
+        runModalCheck:0
+
+
+
+    }
 }
+// modalShowFunction=()=>{
+         
+// }
+     
 
-  changingStateDataFunction=()=>{
+    changingStateDataFunction=(selectedData,selectedData2,selectedData3)=>{
 
-    this.setState=(
-      {
-        SelectedBeastContentFromState:this.state.SelectedBeastContentFromState+"yes i did changing data"
-      }
-    )
-  }
+    //  let newArr= HornedBeasts.find(element=> element.horns===2)
 
+      this.setState(
+        {
+          SelectedBeastContentFromState: selectedData,
+          SelectedBeastContentFromState2:selectedData2,
+          SelectedBeastContentFromState3:selectedData3,
+          runModalCheck:1
+        }
+      )
+      // this.modalShowFunction()
+      
+
+    }
+
+
+ 
 
   render(){
-
     return (
       <>
-       < Header />
-       < Main />
-       <SelectedBeast SelectedBeastContent={this.state.SelectedBeastContentFromState}> </SelectedBeast>
-
-        <Footer />
-    </>
-  
-  )
-  
+         <Header></Header>
+         <Main hornedDataMain={this.state.hornedData} functionProp={this.changingStateDataFunction}></Main>
+         <SelectedBeast SelectedBeastContent={this.state.SelectedBeastContentFromState} SelectedBeastContent2={this.state.SelectedBeastContentFromState2} SelectedBeastContent3={this.state.SelectedBeastContentFromState3} modalShowFunction={this.modalShowFunction}> </SelectedBeast>
+         <Footer></Footer>
+      </>
+    
+    )
+    }
+ 
 }
 
 export default App;
--- src/copmonents/Main.js
+++ src/copmonents/Main.js
@@ -1,28 +1,47 @@
 
 import React from 'react';
 import HornedBeast from './HornedBeast';
-import HornedData from './HornedData.json';
-class Main extends React.Component {
-    prop1Function=()=>{
-        this.props.functionProp();
+// import HornedData from './HornedData.json';
+
+
+
+class Main extends React.Component{
+    
+
+    prop1Function=(selectedData,selectedData2,selectedData3)=>{
+        this.props.functionProp(selectedData,selectedData2,selectedData3);
+        
+        
     }
     
-    render() {
-        return (
-            <>
-                {HornedData.map(item => {
-                    return (
-                        <HornedBeast
-                            title={item.title}
-                            imgUrl={item.image_url}
-                            description={item.description}
-                            prop2Function={this.prop1Function}
-
-                        />
-                    )
-                })
-                }
-            </>
+   
+    render(){
+        return(
+           <>
+           
+           {
+               this.props.hornedDataMain.map(item=>{
+                   return(
+                       <>
+                       <HornedBeast
+                       title={item.title}
+                       image_url={item.image_url}
+                       description={item.description}
+                       prop2Function={()=>this.prop1Function(item.title,item.image_url,item.description)}
+                    //    selectedData={this}
+                       
+                       />
+                       {/* <button onClick={this.prop1Function}>Voteee</button> */}
+                       
+                      
+                       </>
+                   )
+               })
+           }
+           {/* <p>{this.props.propName}</p> */}
+           
+           </>  
+          
         )
     }
 }
